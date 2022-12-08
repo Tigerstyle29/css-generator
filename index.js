bb5ce3739b13d25719254a83c70aae45a35914c5
Tigerstyle29 <tvartanyan29@yandex.ru>
2022-12-08 22:34:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function fun1() {
+    let rtl=document.getElementById('rtl').value;
+    let rtr=document.getElementById('rtr').value;
+    let rbr=document.getElementById('rbr').value;
+    let rbl=document.getElementById('rbl').value;
+    let ttl=document.getElementById('ttl');
+    let ttr=document.getElementById('ttr');
+    let tbr=document.getElementById('tbr');
+    let tbl=document.getElementById('tbl');
+
+    let block = document.getElementById('block');
+    
+    // значение из ползунков в инпут:
+    ttl.value=rtl;
+    ttr.value=rtr;
+    tbr.value=rbr;
+    tbl.value=rbl;
+
+    // задаём блоку радиус скругления:
+    block.style.borderRadius=rtl+'px ' +rtr +'px ' +rbr +'px ' +rbl+'px ';
+}
